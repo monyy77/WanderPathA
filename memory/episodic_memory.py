bea3157665dcd94f5d49a03be4d175a3ce0abdcb
monyy77 <mennasobhe7777@gmail.com>
2026-08-07 15:38:00 +0300

    Implement EpisodicMemory class with basic methods

--- memory/episodic_memory.py
+++ memory/episodic_memory.py
@@ -1,1 +1,28 @@
+from memory.memory_models import Episode
 
+class EpisodicMemory:
+
+    def __init__(self):
+        self.episodes: list[Episode] = []
+
+    def save(self, episode: Episode):
+        self.episodes.append(episode)
+
+    def get_all(self) -> list[Episode]:
+        return list(self.episodes)
+
+    def get_by_entity(
+        self,
+        entity_type: str,
+        entity_id: int
+    ) -> list[Episode]:
+
+        return [
+            episode
+            for episode in self.episodes
+            if episode.entity_type == entity_type
+            and episode.entity_id == entity_id
+        ]
+
+    def clear(self):
+        self.episodes.clear()
